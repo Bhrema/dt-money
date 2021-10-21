82d39975f41b0a63b37d4469be02ba1dee8c7d13
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-10-21 10:01:43 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import Modal from 'react-modal'
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransationModal";
+
+Modal.setAppElement('#root')
+
+export function App() {
+
+  const[isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransaction(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransaction(){
+      setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenTransaction} />
+      <Dashboard />
+
+      <NewTransactionModal 
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransaction}
+      />
+
+      <GlobalStyle />
+    </>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
+import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transations', () => {
+      return [
+        {
+        id:1,
+        title: 'Transation 1',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        CreatedAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
