0723d6c6dd76d3d37388b0b5ffbe74a8ec4c0bcd
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-10-26 09:26:47 -0300

    resolução do bug do axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Lamen",
+          type: "withdraw",
+          category: 'Comida',
+          amount: 50.00,
+          createdAt: new Date('2021-10-25 21:00:00'),
+        },
+        {
+          id: 2,
+          title: "Cartão",
+          type: "withdraw",
+          category: 'Pagamentos',
+          amount: 375.00,
+          createdAt: new Date('2021-10-10 10:00:00'),
+        }
+      ],
+    })
+  },
+
   routes(){
     this.namespace = 'api'
 
     this.get('/transations', () => {
-      return [
-        {
-        id:1,
-        title: 'Transation 1',
-        amount: 400,
-        type: 'deposit',
-        category: 'Food',
-        CreatedAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
